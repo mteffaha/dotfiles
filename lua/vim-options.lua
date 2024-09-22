bf43a701b60953d60b12e7c1c01b64d5ef14443f
Mortadha Teffaha <teffaha.mortadha@gmail.com>
2024-09-22 19:00:46 +0200

    added whichkey and set some navigation options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,10 +1,26 @@
+-- default configs
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
+
+-- folding configuration
+vim.cmd("set nofoldenable")
+vim.cmd("set foldmethod=expr")
+vim.cmd("set foldexpr=v:lua.vim.treesitter.foldexpr()")
+
+-- line number configs
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.signcolumn = "number"
+
+-- mapping leader key
+vim.g.mapleader = " "
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- mapping window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
